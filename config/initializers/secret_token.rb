c580b5f7234b8aa643105f8439049f3402ba1f89
Tony Ng <tonyng222@gmail.com>
2011-08-30 14:07:59 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TonyMe::Application.config.secret_token = '52173b3f8bf1536ff57d523b0ee6c5c284a046abd5223ec1d99bc9b652d51cadbd56809278edb60a62d244d6a4cbfd37fae268d95b715e4fd9a1716247227e4d'
